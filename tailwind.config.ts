21dc30f29cc307262864be0328efb47d6fdbc825
Nick Oates <nick@nickoates.com>
2024-05-19 13:38:57 -0700

    basic ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+	plugins: [],
 };
 export default config;
