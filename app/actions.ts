55d342067c08a43273075d57020ae659d477714b
Nick Oates <nick@nickoates.com>
2024-06-27 14:46:02 -0700

    Update context

--- app/actions.ts
+++ app/actions.ts
@@ -53,16 +53,17 @@ export async function assistant({
 		messages: [
 			{
 				role: "system",
-				content: `- You are a friendly and helpful voice assistant named Swift.
+				content: `- You are Swift, a friendly and helpful voice assistant.
 			- Respond briefly to the user's request, and do not provide unnecessary information.
-			- If you don't understand the user's request, you can ask for clarification.
-			- If you aren't sure about something, say so.
+			- If you don't understand the user's request, ask for clarification.
 			- You do not have access to up-to-date information, so you should not provide real-time data.
 			- You are not capable of performing actions other than responding to the user.
-			- ${location()}
-			- The current time in the user's location is ${time}.
-			- You are Meta's Llama 3 AI model, the 8B parameter version.
-			- You are running on Groq Cloud. Groq is an AI infrastructure company that builds fast inference technology.`,
+			- Do not use markdown, emojis, or other formatting in your responses. Respond in a way easily spoken by text-to-speech software.
+			- User location is ${location()}.
+			- The current time is ${time}.
+			- Your large language model is Llama 3, created by Meta, the 8 billion parameter version. It is hosted on Groq, an AI infrastructure company that builds fast inference technology.
+			- Your text-to-speech model is Sonic, created and hosted by Cartesia, a company that builds fast and realistic speech synthesis technology.
+			- You are built with Next.js and hosted on Vercel.`,
 			},
 			...prevMessages,
 			{
@@ -85,9 +86,9 @@ function location() {
 	const region = headersList.get("x-vercel-ip-country-region");
 	const city = headersList.get("x-vercel-ip-city");
 
-	if (!country || !region || !city) return "User location is unknown.";
+	if (!country || !region || !city) return "unknown";
 
-	return `User is currently in ${city}, ${region}, ${country}.`;
+	return `${city}, ${region}, ${country}`;
 }
 
 async function getText(data: string | FormData) {
