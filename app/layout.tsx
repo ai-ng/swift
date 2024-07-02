a1c99bcf1201054cbfdc8dff5339f026b6d43272
Nick Oates <nick@nickoates.com>
2024-07-01 17:27:54 -0700

    Fix Safari text selection

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,10 @@ export default function RootLayout({
 				className={clsx(
 					GeistSans.variable,
 					GeistMono.variable,
-					"py-8 px-6 lg:p-10 dark:text-white bg-white dark:bg-black min-h-dvh flex flex-col justify-between antialiased font-sans"
+					"py-8 px-6 lg:p-10 dark:text-white bg-white dark:bg-black min-h-dvh flex flex-col justify-between antialiased font-sans select-none"
 				)}
 			>
-				<main className="flex flex-col items-center justify-center grow select-none">
+				<main className="flex flex-col items-center justify-center grow">
 					{children}
 				</main>
 
